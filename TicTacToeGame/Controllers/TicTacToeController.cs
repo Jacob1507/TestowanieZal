8003795e04a3ff262fa53a7b34852f93a06ec9eb
Filip Karczmarz <filipkarczmarz@gmail.com>
2024-05-25 22:19:27 +0200

    Initial commit

---
+++ TicTacToeGame/Controllers/TicTacToeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicTacToeGame.Models;
+
+namespace TicTacToeGame.Controllers
+{
+    public class TicTacToeController : Controller
+    {
+        private static TicTacToe game = new TicTacToe();
+
+        public IActionResult Index()
+        {
+            return View(game);
+        }
+
+        [HttpPost]
+        public IActionResult MakeMove(int index)
+        {
+            game.MakeMove(index);
+            char winner = game.CheckWinner();
+            if (winner != ' ')
+            {
+                ViewBag.Winner = winner;
+                game = new TicTacToe(); // Restart the game
+            }
+            return View("Index", game);
+        }
+    }
+}
---
+++ TicTacToeGame/Models/TicTacToe.cs
@@ -0,0 +1,56 @@
+﻿namespace TicTacToeGame.Models
+{
+    public class TicTacToe
+    {
+        public char[] Board { get; set; }
+        public char CurrentPlayer { get; set; }
+
+        public TicTacToe()
+        {
+            Board = new char[9];
+            for (int i = 0; i < 9; i++)
+            {
+                Board[i] = ' ';
+            }
+            CurrentPlayer = 'X';
+        }
+
+        public bool MakeMove(int index)
+        {
+            if (Board[index] == ' ')
+            {
+                Board[index] = CurrentPlayer;
+                CurrentPlayer = CurrentPlayer == 'X' ? 'O' : 'X';
+                return true;
+            }
+            return false;
+        }
+
+        public char CheckWinner()
+        {
+            int[,] winningCombinations = new int[,]
+            {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8},
+                {0, 3, 6},
+                {1, 4, 7},
+                {2, 5, 8},
+                {0, 4, 8},
+                {2, 4, 6}
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (Board[winningCombinations[i, 0]] != ' ' &&
+                    Board[winningCombinations[i, 0]] == Board[winningCombinations[i, 1]] &&
+                    Board[winningCombinations[i, 0]] == Board[winningCombinations[i, 2]])
+                {
+                    return Board[winningCombinations[i, 0]];
+                }
+            }
+            return ' ';
+        }
+    }
+}
+
